3d3fb65e5353e66a2f5ad27437dbe8ca9c81c828
john jefferson lebasora <johnjefferson.lebasora@evsu.edu.ph>
2024-01-18 12:53:50 +0800

    feat: 404 page

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'forgot-password',component:ForgetPasswordComponent},
-  {path:'reset-password',component:ResetPasswordComponent}
+  {path:'',component:LayoutComponent,children:[
+    {path:'login',component:LoginComponent},
+    {path:'forgot-password',component:ForgetPasswordComponent},
+    {path:'reset-password/:token',component:ResetPasswordComponent},
+    {path:'page-not-found',component:PageNotFoundComponent},
+    {path:'**',redirectTo:"page-not-found"}
+  ]}
+  
 ];
 
 @NgModule({
