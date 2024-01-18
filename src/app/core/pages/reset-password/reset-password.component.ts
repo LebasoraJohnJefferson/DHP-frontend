61f49ae1e5242fd62581554e90758032e16e9539
john jefferson lebasora <johnjefferson.lebasora@evsu.edu.ph>
2024-01-18 12:25:34 +0800

    feature: reset-password

--- src/app/core/pages/reset-password/reset-password.component.ts
+++ src/app/core/pages/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
+  submitLoading: boolean = false;
+  token: any;
 
+  passwordForm: FormGroup = this._fb.group({
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
+
+  constructor(
+    private _fb: FormBuilder,
+    private _route: ActivatedRoute,
+    public router: Router
+  ) {
+    this._route.queryParams.subscribe((value) => {
+      this.token = this._route.snapshot.paramMap.get('token');
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/login']);
+  }
+
+  onSubmit() {
+    if (this.passwordForm.invalid) {
+      // this.toast.warning('Empty Input(s)');
+      this.submitLoading = false;
+    } else if (
+      this.passwordForm.controls['password'].value !=
+      this.passwordForm.controls['confirmPassword'].value
+    ) {
+      // this.toast.error("Password didn't matched.");
+      this.submitLoading = false;
+    } else {
+      // this._authService
+      //   .resetPassword(this.passwordForm.value, this.token)
+      //   .subscribe({
+      //     complete: () => {
+      //       this.submitLoading = false;
+      //       this.passwordForm.reset();
+      //     },
+      //     error: (err) => {
+      //       this.toast.warning(err.error);
+      //       this.submitLoading = false;
+      //     },
+      //     next: (res) => {
+      //       this.toast.success(res.message);
+      //       this.router.navigate(['/login?type=alumni']);
+      //     },
+      //   });
+    }
+  }
 }
