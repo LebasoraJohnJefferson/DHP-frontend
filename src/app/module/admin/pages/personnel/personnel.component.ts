e566adde1c3b83580107bdfb1fbb81da756d318b
Eris <johnjefferson.lebasora@evsu.edu.ph>
2024-01-22 01:21:18 +0800

    style: admin/personnel table

--- src/app/module/admin/pages/personnel/personnel.component.ts
+++ src/app/module/admin/pages/personnel/personnel.component.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './personnel.component.scss'
 })
 export class PersonnelComponent {
-  customers:any=[]
-  selecetedCustomer:any;
-  cols: any[] = [];
+  data:any = [
+    {
+      first_name:"hello wolrd",
+      last_name:"hell world",
+      middle_name:"hell world",
+      email:"hell_world@gmail.com",
+      status:false
+    },
+    {
+      first_name:"hello wolrd",
+      last_name:"hell world",
+      middle_name:"hell world",
+      email:"hell_world@gmail.com",
+      status:false
+    },{
+      first_name:"hello wolrd",
+      last_name:"hell world",
+      middle_name:"hell world",
+      email:"hell_world@gmail.com",
+      status:false
+    }
+  ]
+
+  pages:number[] = [1,3,4,5,6,7]
+
+
 }
