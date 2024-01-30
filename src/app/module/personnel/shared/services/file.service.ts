a4a6f07928fd9cec894ea372b1e4c31617021a5b
Eris <johnjefferson.lebasora@evsu.edu.ph>
2024-01-30 16:33:44 +0800

    feat: uploadfile

---
+++ src/app/module/personnel/shared/services/file.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
+
+import { environment } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
+const BASEURL = environment.baseURL;
+const HELPER = new JwtHelperService();
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FileService {
+  
+    constructor(private http: HttpClient, private router: Router) {}
+  
+    uploadFile(data:any): any {
+      return this.http.post(`${BASEURL}/personnel/file`,data);
+    }
+
+    getFiles(): any {
+      return this.http.get(`${BASEURL}/personnel/file`);
+    }
+
+    deleteFile(id:any):Observable<any>{
+      return this.http.delete(`${BASEURL}/personnel/file/${id}`);
+    }
+
+
+}
