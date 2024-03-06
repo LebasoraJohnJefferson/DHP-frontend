c6ce4dccf5bd1a64024e9138f3d9cfdcf934710c
Eris <johnjefferson.lebasora@evsu.edu.ph>
2024-03-06 17:37:50 +0800

    small fix #3

--- src/app/module/admin/pages/forms/forms.component.ts
+++ src/app/module/admin/pages/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-forms',
@@ -16,13 +16,46 @@ export class FormsComponent {
   ]
 
 
+  selectedMonthRange:any;
+  selectedHeader:string | undefined='Family Profile';
+
+  listOfTitle:any={
+    'infant':'Infant',
+    'baranggay-preschooler':'Barangay Preschool',
+    'list-of-preschooler-with-nutritional-status':'List of Preschool',
+    'at-risk-preschool':'Affected/At Risk'
+  }
+
+  selectedRangedOfMonth:any;
+
+  rangeOfMonth:any={
+    'infant':' (0-23 months old)',
+    'baranggay-preschooler':'Barangay Preschool (3-5 years old)',
+    'list-of-preschooler-with-nutritional-status':'(Identified nutritional status)',
+    'at-risk-preschool':'Affected/At Risk  (0-59 months old)'
+  }
+
+  
+
+
   constructor(
-    public router:Router
+    public router:Router,
+    public route: ActivatedRoute
   ){
 
   }
 
-
+  ngDoCheck(): void{
+    const route = this.route.snapshot.children[0].routeConfig?.path
+    if(route){
+      this.selectedHeader = this.listOfTitle[route];
+      this.selectedMonthRange = this.rangeOfMonth[route];
+    } 
+    else{
+      this.selectedMonthRange = '';
+      this.selectedHeader = 'Family Profile'
+    }
+  }
 
   navigateTo(link:any){
     this.router.navigate([link?.target?.value])
